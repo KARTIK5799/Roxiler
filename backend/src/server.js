32740b0d03fd3c8a46291a2ab1560f33e6734ad0
KARTIK5799 <thekartiksapkal@gmail.com>
2025-02-05 20:56:08 +0530

    feat:server:configure server file to run the server by importing app file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,9 @@
-hi
+import express from "express";
+import app from './app.js'
+
+const port = process.env.PORT || 7000; 
+
+
+app.listen(port,()=>{
+    console.log(`Server Is Running On ${port}`);
+})
