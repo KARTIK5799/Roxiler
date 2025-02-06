561246ee55b989dd5c45e3cc77ea7c87216522cd
KARTIK5799 <thekartiksapkal@gmail.com>
2025-02-06 22:14:09 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import app from './src/app.js'
 
 const port = process.env.PORT || 7000; 
 
-
+app.use("/",(req,res)=>{
+    res.send("hello")
+})
 app.listen(port,()=>{
     console.log(`Server Is Running On ${port}`);
 })
